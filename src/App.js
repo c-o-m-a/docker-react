86726c527971016bbf7a864d1b93a760c7f0ae0a
Marjan Erceg <marjanerceg@yahoo.com>
2020-04-17 14:35:39 +0200

    changed app text

--- src/App.js
+++ src/App.js
@@ -8,18 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-
-          cao
-          Edit <code>src/App.js</code> and save to reload.
+dsdsd
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
       </header>
     </div>
   );
